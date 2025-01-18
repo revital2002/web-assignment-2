e73df6ec89ec120c96510d5d65892b721063225f
Dana Cohen <dana.co@moonactive.com>
2025-01-18 12:29:05 +0200

    first init

---
+++ index.ts
@@ -0,0 +1,46 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import dotenv from 'dotenv';
+import swaggerSetup from './swagger';
+import routes from './routes/index';
+
+dotenv.config({ path: path.join(__dirname, './.env') });
+process.env.rootDir = __dirname;
+
+const PORT = process.env.PORT;
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', routes);
+
+// Swagger documentation setup
+swaggerSetup(app);
+
+export const startServer = async (): Promise<ReturnType<Express['listen']> | null> => {
+  try {
+    console.log('\nTrying to connect to MongoDB...');
+    await mongoose.connect(process.env.DB_CONNECT!);
+    console.log('MongoDB connected successfully');
+  } catch (exception: any) {
+    console.error(exception.message);
+    console.error(exception.stack);
+    throw exception;
+  }
+
+  return app.listen(PORT, () => {
+    console.log(`\nServer is listening on port: ${PORT} \n`);
+    console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
+  });
+};
+
+export const maybeStartServer = (): void => {
+  if (process.env.NODE_ENV !== 'test') {
+    startServer();
+  }
+};
+
+maybeStartServer();
+
+export default app;
